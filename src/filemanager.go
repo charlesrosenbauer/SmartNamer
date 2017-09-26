0b7ab98312d1967116175520a59488c76d30f1af
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-09-26 23:45:24 +0000

    Added a function to extract identifiers from files

--- src/filemanager.go
+++ src/filemanager.go
@@ -4,7 +4,8 @@ package main
 
 import (
   "io/ioutil"
-  //"regexp"
+  "regexp"
+  "errors"
 )
 
 
@@ -48,73 +49,73 @@ func loadSourceFiles (files []string) ([]string, error) {
 */
 var extMap = map [string] string {
   // C/C++
-  ".c"    : "[a-zA-Z\\d_]+",
-  ".h"    : "[a-zA-Z\\d_]+",
-  ".cc"   : "[a-zA-Z\\d_]+",
-  ".hh"   : "[a-zA-Z\\d_]+",
-  ".cpp"  : "[a-zA-Z\\d_]+",
-  ".hpp"  : "[a-zA-Z\\d_]+",
+  ".c"    : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".h"    : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".cc"   : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".hh"   : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".cpp"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".hpp"  : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Haskell
-  ".hs"   : "[a-zA-Z\\d]+",
-  ".lhs"  : "[a-zA-Z\\d]+",
+  ".hs"   : "[a-zA-Z\\d]+'*",
+  ".lhs"  : "[a-zA-Z\\d]+'*",
 
   //My personal language Bzo, because why the hell not?
-  ".bz"   : "[^ \\$ \\( \\) \\] \\[ \\{ \\}: ; \\. \\, \\_ \\` \\\" \\@ ]+",
-  ".lbz"  : "[^ \\$ \\( \\) \\] \\[ \\{ \\}: ; \\. \\, \\_ \\` \\\" \\@ ]+",
+  ".bz"   : "[^ $ \\( \\) \\] \\[ \\{ \\} :; \\. \\, \\_ ` \" @ ']+'*",
+  ".lbz"  : "[^ $ \\( \\) \\] \\[ \\{ \\} :; \\. \\, \\_ ` \" @ ']+'*",
 
   //Jai, because why the hell not?
-  ".jai"  : "[a-zA-Z\\d_]+",
+  ".jai"  : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Python
-  ".py"   : "[a-zA-Z\\d_]+",
-  ".pyc"  : "[a-zA-Z\\d_]+",
-  ".pyd"  : "[a-zA-Z\\d_]+",
-  ".pyo"  : "[a-zA-Z\\d_]+",
-  ".pyw"  : "[a-zA-Z\\d_]+",
-  ".pyz"  : "[a-zA-Z\\d_]+",
+  ".py"   : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".pyc"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".pyd"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".pyo"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".pyw"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".pyz"  : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Javascript
-  ".js"   : "[a-zA-Z\\d_]+",
+  ".js"   : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Golang
-  ".go"   : "[a-zA-Z\\d_]+",
+  ".go"   : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Rust
-  ".rs"   : "[a-zA-Z\\d_]+",
-  ".rlib" : "[a-zA-Z\\d_]+",
+  ".rs"   : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".rlib" : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //C#
-  ".cs"   : "[a-zA-Z\\d_]+",
+  ".cs"   : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Elm
-  ".elm"  : "[a-zA-Z\\d_]+",
+  ".elm"  : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Clojure
-  ".clj"  : "[^ \\( \\) \\] \\[ \\} \\{ \\;]+",
-  ".cljs" : "[^ \\( \\) \\] \\[ \\} \\{ \\;]+",
-  ".cljc" : "[^ \\( \\) \\] \\[ \\} \\{ \\;]+",
-  ".edn"  : "[^ \\( \\) \\] \\[ \\} \\{ \\;]+",
+  ".clj"  : "[^ \\( \\) \\] \\[ \\} \\{ ;]+",
+  ".cljs" : "[^ \\( \\) \\] \\[ \\} \\{ ;]+",
+  ".cljc" : "[^ \\( \\) \\] \\[ \\} \\{ ;]+",
+  ".edn"  : "[^ \\( \\) \\] \\[ \\} \\{ ;]+",
 
   //Kotlin
-  ".kt"  : "[a-zA-Z\\d_]+",
-  ".kts" : "[a-zA-Z\\d_]+",
+  ".kt"  : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".kts" : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Idris
-  ".idr" : "[a-zA-Z\\d_]+",
-  ".lidr": "[a-zA-Z\\d_]+",
+  ".idr" : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".lidr": "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Erlang
-  ".erl" : "[a-zA-Z\\d_]+",
-  ".hlr" : "[a-zA-Z\\d_]+",
+  ".erl" : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".hlr" : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //Scala
-  ".scala": "[a-zA-Z\\d_]+",
-  ".sc"   : "[a-zA-Z\\d_]+",
+  ".scala": "[a-zA-Z][a-zA-Z\\d_]+",
+  ".sc"   : "[a-zA-Z][a-zA-Z\\d_]+",
 
   //ML / Caml / OCaml
-  ".ml"   : "[a-zA-Z\\d_]+",
-  ".mli"  : "[a-zA-Z\\d_]+",
+  ".ml"   : "[a-zA-Z][a-zA-Z\\d_]+",
+  ".mli"  : "[a-zA-Z][a-zA-Z\\d_]+",
 }
 
 
@@ -125,18 +126,25 @@ var extMap = map [string] string {
 
 
 
-/* !!HARD HAT ZONE!!
-func getIds (reg, text, fname string) ([]string, error) {
-  failArr := []string{}
 
-  regex, err0 := regexp.Compile(reg)
-  if err0 == nil {
-    return failArr, err0
-  }
+func getIds (text, fname string) ([]string, error) {
+  failArr := []string{}
 
   langRegex := regexp.MustCompile("\\.[a-zA-Z]+")
   lang := langRegex.FindString(fname)  //get file extension to figure out the language
 
+  reg, ok := extMap[lang]
+  if ! ok {
+    return failArr, errors.New("Unrecognized File Extension: " + lang)
+  }
+
+  regex, err0 := regexp.Compile(reg)
+  if err0 != nil {
+    return failArr, err0
+  }
+
+  ids := regex.FindAllString(text, -1)
+
+  return ids, nil
 
 }
-*/
--- src/main.go
+++ src/main.go
@@ -20,12 +20,31 @@ func main() {
   vect1  = representPosition("main.go", 115, vect1)
   fmt.Println(vectToString(vectIntersection(vect0, vect1)))
 
-  fs, err := loadSourceFiles([]string{"src/main.go", "src/filemanager.go", "src/folding.go"})
-  if err != nil {
-    fmt.Println(err)
+  fnames := []string{"src/main.go", "src/filemanager.go", "src/folding.go"}
+  fs0, err0 := loadSourceFiles(fnames)
+  if err0 != nil {
+
+    fmt.Println(err0)
+
   }else{
-    for i:=0; i<len(fs); i++ {
-      fmt.Println(fs[i])
+
+    var fs1 [][]string
+
+    for i:=0; i<len(fs0); i++ {
+      fsTemp, err1 := getIds(fs0[i], fnames[i])
+
+      if err1 != nil {
+        fmt.Println(err1)
+      }else{
+        fs1 = append(fs1, fsTemp)
+      }
     }
+
+    for i:=0; i<len(fs1); i++ {
+      fmt.Println(fs1[i])
+    }
+
   }
+
+
 }
