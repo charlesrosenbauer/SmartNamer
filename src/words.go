069f154cb00396a6ed435868ddfc417405c9111d
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-10-02 23:39:18 +0000

    Started moving word list into .go file

---
+++ src/words.go
@@ -0,0 +1,44 @@
+package main
+
+
+
+
+
+
+
+
+
+
+var words []string = []string{ "map", "filter", "reduce", "fold", "scan",
+  "bind", "zip", "unzip", "define", "either", "maybe", "functor", "applicative",
+  "monad", "promise", "future", "apply", "call", "base", "identity", "identify",
+  "compose", "query", "search", "watch", "show", "able", "unable", "usable",
+  "display", "draw", "compile", "create", "form", "delta", "compress",
+  "compressing", "compressor", "for", "while", "name", "identifier", "fail",
+  "failure", "test", "error", "signal", "exception", "except", "accept",
+  "catch", "catcher", "throw", "thrower", "pass", "succeed", "socket", "lead",
+  "leave", "leaving", "quit", "mail", "email", "format", "print", "fetch",
+  "goal", "mark", "anneal", "annealing", "annealer", "rate", "rating", "pack",
+  "package", "repo", "repository", "string", "bool", "boolean", "int", "integer",
+  "float", "floating", "point", "double", "set", "hashmap", "matrix", "learn",
+  "learning", "tensor", "deep", "order", "fizz", "buzz", "beep", "sound",
+  "play", "stop", "clock", "pause", "use", "using", "done", "complete",
+  "completed", "resume", "trigger", "represent", "insert", "remove", "trade",
+  "exchange", "change", "report", "repair", "mix", "id", "bin", "binary", "hex",
+  "handle", "handler", "smart", "get", "set", "alloc", "allocate", "pointer",
+  "array", "hash", "len", "length", "size", "import", "importer", "struct",
+  "specific", "specify", "spec", "factor", "find", "replace", "attach",
+  "connect", "standard", "std", "my", "your", "me", "you", "all", "none", "yet",
+  "swap", "quick", "rapid", "insertion", "bubble", "bogo", "sort", "view",
+  "user", "users", "if", "then", "else", "gate", "race", "condition", "compare",
+  "comp", "load", "store", "cache", "recog", "recognition", "recognize",
+  "recognizer", "match", "matcher", "pattern", "dictionary", "dict", "diction",
+  "table", "array", "list", "link", "graph", "tree", "dag", "ast", "syntax",
+  "add", "subtract", "sub", "multiply", "mul", "divide", "div", "sum",
+  "product", "quotient", "remainder", "remain", "modulus", "mod", "addition",
+  "additional", "vector", "matrix", "cross", "dot", "volume", "perimeter",
+  "area", "height", "width", "depth", "associate", "associative", "commute",
+  "commutative", "algebra", "calculus", "category", "theory", "info",
+  "information", "entropy", "entropic", "trig", "sine", "cosine", "tangent",
+  "arc", "sin", "cos", "tan", "count", "accum", "accumulate", "arc", "arch",
+  "parabola", "parabolic" }
